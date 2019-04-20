6a70c2a2436ffea827d25eb1e8976967c647ed6a
johnoro <officialjohnorourke@gmail.com>
2019-04-19 20:26:46 -0400

    Completes args.py

--- src/args.py
+++ src/args.py
@@ -5,6 +5,12 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(addend1, addend2):
+  try:
+    return addend1 + addend2
+  except ValueError:
+    print('Your supplied addends could not be summed.')
+    return None
 
 print(f1(1, 2))
 
@@ -12,6 +18,20 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*nums):
+  numLen = len(nums)
+  if numLen == 0:
+    return 0
+  if numLen == 1:
+    return nums[0]
+  try:
+    sum = 0
+    for n in nums:
+      sum += n
+    return sum
+  except:
+    print('Your supplied arguments could not be summed.')
+    return None
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +41,15 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(addend1, addend2 = 1):
+  return f1(addend1, addend2)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +64,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**keywordArgs):
+  for k, v in keywordArgs.items():
+    print(f'key: {k}, value: {v}')
 
 # Should print
 # key: a, value: 12
@@ -55,9 +80,9 @@ f4(a=12, b=30)
 f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
 d = {
-    "monster": "goblin",
-    "hp": 3
+  "monster": "goblin",
+  "hp": 3
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)

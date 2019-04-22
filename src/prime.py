9b5ec8d153ee06ab1ad4c79bb195afb1e03f7112
johnoro <officialjohnorourke@gmail.com>
2019-04-22 10:28:30 -0400

    Adds continuous loop for checking primes

--- src/prime.py
+++ src/prime.py
@@ -9,10 +9,9 @@ from math import sqrt, ceil
 arePrimes = [0, 1]
 
 def primeList(end):
-  start = len(arePrimes)
-  for i in range(start, end+1):
+  for i in range(len(arePrimes), end+1):
     arePrimes.append(True)
-  for i in range(start, ceil(sqrt(end))):
+  for i in range(2, ceil(sqrt(end)) + 1):
     if arePrimes[i]:
       for j in range(i*i, end+1, i):
         arePrimes[j] = False
@@ -24,22 +23,25 @@ def getNum():
   return int(num)
 
 
-try:
-  num = getNum()
-  def printPrime(): print(f'{num} is prime.')
-  def printNotPrime(): print(f'{num} is not prime.')
+print('\nNon-numbers, e.g. \'q\', can be used to exit.')
+while True:
+  try:
+    num = getNum()
+    def printPrime(): print(f'{num} is prime.')
+    def printNotPrime(): print(f'{num} is not prime.')
 
-  if num > 1:
-    if num >= len(arePrimes):
-      primeList(num)
+    if num > 1:
+      if num >= len(arePrimes):
+        primeList(num)
 
-    if arePrimes[num]:
-      printPrime()
+      if arePrimes[num]:
+        printPrime()
+      else:
+        printNotPrime()
     else:
       printNotPrime()
-  else:
-    printNotPrime()
-except ValueError:
-  print('It seems that you did not enter a number.')
-
-print()
+    
+    print()
+  except ValueError:
+    print('It seems that you did not enter a number.\n')
+    break
